013b77eb8918deb6b31aa6f3adde2b0403ac74b9
JeremyWesthead <jeremywesthead3@gmail.com>
2024-07-19 10:24:31 +0100

    docs: expand module level docstring

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,33 @@
 //! - Gene representation
 //! - VCF file parsing
 //! - Finding effects of a given VCF file at both genome and gene levels
+//! 
+//! # Example
+//! ```
+//! use grumpy::genome::{Genome, mutate};
+//! use grumpy::vcf::VCFFile;
+//! use grumpy::difference::{GenomeDifference, GeneDifference};
+//! 
+//! let reference = Genome::new("some/path/to/a/genbank/file.gb");
+//! let vcf = VCFFile::new("some/path/to/a/vcf/file.vcf");
+//! let sample = mutate(&reference, vcf);
+//! 
+//! let genome_diff = GenomeDifference::new(reference, sample);
+//! for variant in genome_diff.variants.iter(){
+//!    println!("{}", variant.variant);
+//! }
+//! 
+//! for gene_name in sample.genes_with_mutations.clone().iter(){
+//!   let gene_diff = GeneDifference::new(reference.get_gene(gene_name.clone()), sample.get_gene(gene_name.clone()));
+//!   for mutation in gene_diff.mutations.iter(){
+//!     println!("{}", mutation.mutation);
+//!   }
+//! }
+//! 
+//! ```
+//! 
+//! Also provides an interface to this library as a Python module using PyO3.
+//! `pip install bio-grumpy`
 use pyo3::prelude::*;
 
 pub mod common;
